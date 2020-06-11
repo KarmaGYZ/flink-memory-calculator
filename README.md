dd099eeaa527e97052372559a781c9598444a87a
Yangze Guo <karmagyz@gmail.com>
2020-06-11 14:41:38 +0800

    Add support to Flink-1.11

--- README.md
+++ README.md
@@ -1,8 +1,11 @@
 # flink-memory-calculator
-A third party tool to simulate the calculation result of Flink's memory configuration. Only valid for Flink-1.10.
+A third party tool to simulate the calculation result of Flink's memory configuration.
+
+  - calculator-1.10.sh for Flink-1.10.
+  - calculator-1.11.sh for Flink-1.11.
 
 Usage:
-Add the `calculator.sh` to the `FLINK_DIST/bin`. You should set all configurations in your `FLINK_CONF_DIR` and then execute `bin/calculator.sh`. You could also append dynamic config options to it.
+Add the `calculator-{1.10 or 1.11}.sh` to the `FLINK_DIST/bin`. You should set all configurations in your `FLINK_CONF_DIR` and then execute `bin/calculator-{1.10 or 1.11}.sh`. You could also append dynamic config options to it.
 
 If you want to use "-tm", please repalce it with "-Dtaskmanager.memory.process.size=". This config would be deprecated eventually.
 
@@ -26,7 +29,7 @@ TaskManager Dynamic Configs:
     taskmanager.memory.task.off-heap.size=0b
 ```
 
-We also provide human-readable mode, you could switch to this mode by executing `bin/calculator.sh -h`. In this mode, all the memory size would be converted to a human-readable string. Note that there could be precision loss in converting. For the above sample, it would be:
+We also provide human-readable mode, you could switch to this mode by executing `bin/calculator-{1.10 or 1.11}.sh -h`. In this mode, all the memory size would be converted to a human-readable string. Note that there could be precision loss in converting. For the above sample, it would be:
 
 ```
 JVM Parameters:
---
+++ calculator-1.11.sh
@@ -0,0 +1,94 @@
+#!/usr/bin/env bash
+################################################################################
+#  Licensed to the Apache Software Foundation (ASF) under one
+#  or more contributor license agreements.  See the NOTICE file
+#  distributed with this work for additional information
+#  regarding copyright ownership.  The ASF licenses this file
+#  to you under the Apache License, Version 2.0 (the
+#  "License"); you may not use this file except in compliance
+#  with the License.  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+# limitations under the License.
+################################################################################
+
+# This script only suit for Flink-1.10.
+# Usage: calculator.sh [-h] [-D args]
+#        -h human readable mode
+
+bin=`dirname "$0"`
+bin=`cd "$bin"; pwd`
+
+. "$bin"/config.sh
+
+if [ "$1" == "-h" ]; then
+    HUMAN_READABLE="human"
+    shift
+    echo "There may be precision loss in human readable mode."
+fi
+
+ARGS="${@:1}"
+
+translate_to_human_readable() {
+    origin_param=$1
+    if [ $(echo $origin_param | grep -c 'taskmanager.cpu.cores') != "0" ]; then
+        echo $origin_param
+    else
+        size=$(echo $origin_param |grep -o '[0-9]\+')
+        if (( size < 1024 )); then
+            echo $origin_param | sed 's/b$//g' | sed 's/$/b/g'
+        elif (( size < 1048576 )); then
+            size_in_kb=$(( $size / 1024 ))
+            echo $origin_param | sed "s/$size/$size_in_kb/g" | sed 's/b$//g' | sed 's/$/kb/g'
+        elif (( size < 1073741824 )); then
+            size_in_mb=$(( $size / 1048576 ))
+            echo $origin_param | sed "s/$size/$size_in_mb/g" | sed 's/b$//g' | sed 's/$/mb/g'
+        else
+            size_in_gb=$(( $size / 1073741824 ))
+            echo $origin_param | sed "s/$size/$size_in_gb/g" | sed 's/b$//g' | sed 's/$/g/g'
+        fi
+    fi
+}
+
+java_utils_output=$(runBashJavaUtilsCmd GET_TM_RESOURCE_PARAMS "${FLINK_CONF_DIR}" "$FLINK_BIN_DIR/bash-java-utils.jar:$(findFlinkDistJar)" "${ARGS[@]}")
+logging_output=$(extractLoggingOutputs "${java_utils_output}")
+params_output=$(extractExecutionResults "${java_utils_output}" 2)
+
+if [[ $? -ne 0 ]]; then
+    echo "[ERROR] Could not get JVM parameters and dynamic configurations properly."
+    exit 1
+fi
+
+jvm_params=$(echo "${params_output}" | head -n 1)
+IFS=$" " dynamic_configs=$(echo "${params_output}" | tail -n 1)
+
+echo "JVM Parameters:"
+for param in $(echo "$jvm_params")
+do
+    if [ -z $HUMAN_READABLE ]; then
+        echo "    $param"
+    else
+        human_readable_param=$(translate_to_human_readable $param)
+        echo "    $human_readable_param"
+    fi
+done
+
+echo ""
+
+dynamic_configs=$(echo $dynamic_configs | sed 's/-D //g')
+echo "TaskManager Dynamic Configs:"
+for config in $(echo "$dynamic_configs")
+do
+    if [ -z $HUMAN_READABLE ]; then
+        echo "    $config"
+    else
+        human_readable_config=$(translate_to_human_readable $config)
+        echo "    $human_readable_config"
+    fi
+done
+
